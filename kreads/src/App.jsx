c53f412a8fd13a461603c5c8fd95a261f605afb9
muriungiMartin <muriungimmwiti@gmail.com>
2024-02-28 17:05:44 +0300

    frontend challlenge 1

---
+++ kreads/src/App.jsx
@@ -0,0 +1,12 @@
+import Main from './components/main'
+
+export default function App() {
+  return (
+    // <h1 className="text-3xl font-bold underline">
+    //   Hello world!
+    // </h1>
+    <div className="bg-gray-100">
+      <Main />
+    </div>
+  )
+}
---
+++ kreads/src/components/main.jsx
@@ -0,0 +1,54 @@
+const Main = () => {
+  return (
+    <div className="w-full h-screen bg-neutral-900">
+      <div className="p-16">
+        <div className="w-1/5 flex flex-col justify items-center space-y-2 bg-neutral-800 rounded-lg">
+          <div className="py-2 px-8 pt-8">
+            <img
+              className="rounded-full"
+              src="/avatar-jessica.jpeg"
+              alt="pxaceholder"
+            />
+          </div>
+          <div className="py-2 px-8">
+            <p className="text-2xl font-bold text-neutral-100">
+              Jessica Randall
+            </p>
+            <p className="pl-2 text-sm text-lime-400">London, United Kingdom</p>
+          </div>
+          <div className="py-2 px-8 text-sm">
+            <p className="text-neutral-100">
+              Front-end developer and avid reader.
+            </p>
+          </div>
+          <div className="w-full px-8">
+            <button className="w-full bg-primary-500 py-3 px-8 bg-neutral-700 text-sm font-bold text-neutral-100 rounded-lg hover:bg-lime-400 hover:text-black">
+              GitHub
+            </button>
+          </div>
+          <div className="w-full px-8">
+            <button className="w-full bg-primary-500 py-3 px-8 bg-neutral-700 text-sm font-bold  text-neutral-100 rounded-lg hover:bg-lime-400 hover:text-black">
+              Frontend Mentor
+            </button>
+          </div>
+          <div className="w-full px-8">
+            <button className="w-full bg-primary-500 py-3 px-8 bg-neutral-700 text-sm font-bold  text-neutral-100 rounded-lg hover:bg-lime-400 hover:text-black">
+              LinkedIn
+            </button>
+          </div>
+          <div className="w-full px-8">
+            <button className="w-full bg-primary-500 py-3 px-8 bg-neutral-700 text-sm font-bold  text-neutral-100 rounded-lg hover:bg-lime-400 hover:text-black">
+              Twitter
+            </button>
+          </div>
+          <div className="w-full px-8 pb-16">
+            <button className="w-full bg-primary-500 py-3 px-8 bg-neutral-700 text-sm font-bold  text-neutral-100 rounded-lg hover:bg-lime-400 hover:text-black">
+              Instagram
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Main;
